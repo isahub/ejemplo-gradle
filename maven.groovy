69cd8203df6cd2b29d235a344c01ed8414ef3ec8
isahub <isabel.villavicencio@gmail.com>
2022-11-21 16:43:00 -0300

    j

--- maven.groovy
+++ maven.groovy
@@ -1,9 +1,9 @@
-def maven_build_test() {
+def buildMaven() {
     echo 'maven_build_test ./mvn clean install -e'
     sh './mvnw clean install -e'
 }
 
-def buildMaven() {
+def buildMaven2() {
     sh './mvnw clean compile -e'
     sh './mvnw clean test -e'
     sh './mvnw clean package -e'           
