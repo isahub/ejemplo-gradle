d16b37b6ef74b0b5cfc1daeb4adec126fea5b70f
isahub <isabel.villavicencio@gmail.com>
2022-11-21 17:36:41 -0300

    i

--- maven.groovy
+++ maven.groovy
@@ -17,8 +17,7 @@ def sonarMaven() {
 def runTestMaven() {
     sh './mvnw spring-boot:run &'
     sh 'sleep 5'
-    echo 'Testing in progress.....'
-    sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    //sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     sh 'sleep 5'
     def response = sh(script: "echo \$(curl --write-out '%{http_code}' --silent --output /dev/null http://localhost:8081/rest/mscovid/test?msg=testing)", returnStdout: true);   
     echo "status ${response}"
